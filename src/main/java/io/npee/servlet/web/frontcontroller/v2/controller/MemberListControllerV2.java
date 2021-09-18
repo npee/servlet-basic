bae161f9b582201053df891d6c2a0884dc88e202
npee <jmc4664@gmail.com>
2021-09-18 13:09:57 +0900

    MVC 프레임워크 만들기 - v2 - View 분리

---
+++ src/main/java/io/npee/servlet/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,27 @@
+package io.npee.servlet.web.frontcontroller.v2.controller;
+
+import io.npee.servlet.domain.member.Member;
+import io.npee.servlet.domain.member.MemberRepository;
+import io.npee.servlet.web.frontcontroller.MyView;
+import io.npee.servlet.web.frontcontroller.v1.ControllerV1;
+import io.npee.servlet.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
+
+        return new MyView("/WEB-INF/views/members.jsp");
+    }
+}
