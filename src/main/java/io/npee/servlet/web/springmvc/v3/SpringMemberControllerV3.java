a6b0109c103f7dc45ed871b8302001dadd8f05d3
npee <jmc4664@gmail.com>
2021-09-23 19:19:53 +0900

    스프링 MVC - v3 - 실용적인 방식

---
+++ src/main/java/io/npee/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,47 @@
+package io.npee.servlet.web.springmvc.v3;
+
+import io.npee.servlet.domain.member.Member;
+import io.npee.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public String newForm() {
+        return "new-form";
+    }
+
+    @RequestMapping("/save")
+    public String save(@RequestParam String username,
+                       @RequestParam int age,
+                       Model model) {
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.addAttribute("member", member);
+        return "save-result";
+    }
+
+    @RequestMapping
+    public String members(Model model) {
+
+        List<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+
+        return "members";
+    }
+
+}
